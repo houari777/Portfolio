2d7e868172bba4ae0fadc17d36342d9c4849ab84
H.DJEBBOURI <H.DJEBBOURI@anem.dz>
2025-05-25 13:57:15 +0100

    Add CNAME file for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
